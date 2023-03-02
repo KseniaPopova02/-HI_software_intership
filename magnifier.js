02b7d8b14746d5cf79c03ed345fc97dafd773218
Ksenia <kseniapopova02@gmail.com>
2023-03-02 22:06:24 +0100

    fix all bugs, except fucking input

--- magnifier.js
+++ magnifier.js
@@ -5,5 +5,21 @@ const button2 = document.getElementById("burgerMagnifier");
 const input = document.getElementById("headerInput");
 const input2 = document.getElementById("burgerInput");
 
+const changePage = (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault;
+    window.location.href = "./tags.html";
+  }
+};
+
+const getValue = () => {
+  searchInput.value = input.value;
+};
+
 button.addEventListener("click", toggleFunction(input, "show__input"));
 button2.addEventListener("click", toggleFunction(input2, "show__input"));
+
+input.addEventListener("keypress", changePage);
+input2.addEventListener("keypress", changePage);
+
+input.addEventListener("input", getValue);
--- tags.js
+++ tags.js
@@ -1,35 +1,52 @@
-document.getElementById("searchInput").oninput = function () {
-  const val = this.value.trim();
-  const searchItems = document.querySelectorAll(".search__tags li");
-  if (val) {
-    searchItems.forEach(function (elem) {
-      if (elem.innerText.search(val) == -1) {
-        elem.classList.add("hide");
-        elem.innerHTML = elem.innerText;
-      } else {
-        elem.classList.remove("hide");
-        const str = elem.innerText;
-        elem.innerHTML = insertMark(
-          str,
-          elem.innerText.search(val),
-          val.length
-        );
-      }
-    });
-  } else {
-    searchItems.forEach(function (elem) {
-      elem.classList.remove("hide");
-      elem.innerHTML = elem.innerText;
-    });
-  }
+// document.getElementById("searchInput").oninput = function () {
+//   const val = this.value.trim();
+//   const searchItems = document.querySelectorAll(".search__tags li");
+//   if (val) {
+//     searchItems.forEach(function (elem) {
+//       if (elem.innerText.search(val) == -1) {
+//         elem.classList.add("hide");
+//         elem.innerHTML = elem.innerText;
+//       } else {
+//         elem.classList.remove("hide");
+//         const str = elem.innerText;
+//         elem.innerHTML = insertMark(
+//           str,
+//           elem.innerText.search(val),
+//           val.length
+//         );
+//       }
+//     });
+//   } else {
+//     searchItems.forEach(function (elem) {
+//       elem.classList.remove("hide");
+//       elem.innerHTML = elem.innerText;
+//     });
+//   }
+// };
+
+// const insertMark = (string, pos, len) => {
+//   return (
+//     string.slice(0, pos) +
+//     "<mark>" +
+//     string.slice(pos, pos + len) +
+//     "</mark>" +
+//     string.slice(pos + len)
+//   );
+// };
+
+const searchInput = document.getElementById("searchInput");
+const searchTags = document.querySelectorAll(".search__tags li");
+
+const filterList = () => {
+  const filter = searchInput.value.toLowerCase();
+  searchTags.forEach((item) => {
+    let text = item.textContent;
+    if (text.toLocaleLowerCase().includes(filter.toLocaleLowerCase())) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
+    }
+  });
 };
 
-const insertMark = (string, pos, len) => {
-  return (
-    string.slice(0, pos) +
-    "<mark>" +
-    string.slice(pos, pos + len) +
-    "</mark>" +
-    string.slice(pos + len)
-  );
-};
+searchInput.addEventListener("input", filterList);
