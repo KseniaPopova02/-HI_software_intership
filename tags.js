350dc67ddbb96f1eb6ea48f190a4aa749b640c88
Ksenia <kseniapopova02@gmail.com>
2023-03-06 13:25:37 +0100

    fix: all bugs

--- tags.js
+++ tags.js
@@ -1,28 +1,74 @@
+//Option with local storage
+
+// const searchInput = document.getElementById("searchInput");
+// const searchTags = document.querySelectorAll(".search__tags li");
+
+// const filterList = (filter) => {
+//   searchTags.forEach((item) => {
+//     text = item.textContent;
+
+//     if (text.toLocaleLowerCase().includes(filter.toLocaleLowerCase())) {
+//       item.style.display = "";
+//     } else {
+//       item.style.display = "none";
+//     }
+//   });
+//   localStorage.clear();
+// };
+
+// searchInput.addEventListener("input", (event) => {
+//   filterList(event.target.value);
+// });
+
+// const getItemFromLocalStorage = () => {
+//   return localStorage.getItem("input");
+// };
+
+// const savedValue = getItemFromLocalStorage();
+// searchInput.value = savedValue;
+// filterList(savedValue);
+
+//Option without local storage
+
 const searchInput = document.getElementById("searchInput");
 const searchTags = document.querySelectorAll(".search__tags li");
 
-const filterList = (filter) => {
-  searchTags.forEach((item) => {
-    let text = item.textContent;
+const insertMark = (string, value) =>
+  string.replace(
+    new RegExp(`([\s\S])*(${value})([\s\S])*`, "g"),
+    "$1<mark>$2</mark>$3"
+  );
 
-    if (text.toLocaleLowerCase().includes(filter.toLocaleLowerCase())) {
-      item.style.display = "";
+const filterList = (filter) => {
+  searchTags.forEach((elem) => {
+    const text = elem.textContent;
+
+    if (!text.toLocaleLowerCase().includes(filter.toLocaleLowerCase())) {
+      elem.classList.add("hide");
+      //?????
+      elem.innerHTML = elem.innerText;
     } else {
-      item.style.display = "none";
+      elem.classList.remove("hide");
+
+      const str = elem.innerText;
+      const result = insertMark(str, filter);
+
+      elem.innerHTML = result;
     }
   });
 };
 
-searchInput.addEventListener("input", filterList);
+searchInput.addEventListener("input", (event) =>
+  filterList(event.target.value)
+);
 
-const getItemFromLocalStorage = () => {
-  localStorage.getItem("input");
+const getSearchItem = () => {
+  const queryParams = window.location.search.slice(1).split("=");
+  return queryParams[1] || "";
 };
 
-const savedValue = getItemFromLocalStorage();
-searchInput.value = savedValue;
-filterList(savedValue);
+let savedValue = getSearchItem();
 
-// window.addEventListener("onload", () => {
-//   localStorage.clear();
-// });
+searchInput.value = savedValue;
+
+filterList(savedValue);
